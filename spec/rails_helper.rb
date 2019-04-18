3fb650a4395e354831abc11b888b7614b67e0faa
AlexDNZ <paleozene@gmail.com>
2019-04-18 15:25:05 +0200

    Rectify .gitignore file Rectify rails_helper.rb  : on line is moved down like Zaratan suggested

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,9 +14,8 @@ require 'rspec/rails'
 require 'simplecov'
 SimpleCov.start
 
-Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
-
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
