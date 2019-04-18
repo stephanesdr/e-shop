934fd99910f2bb96877bed0758cd8a58387bfad4
MargVander <margauxvanderbeken@gmail.com>
2019-04-18 16:19:45 +0200

    test for category

---
+++ spec/factories/categories.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+
+FactoryBot.define do
+  factory :category do
+    name             { Faker::Food.dish }
+  end
+end
