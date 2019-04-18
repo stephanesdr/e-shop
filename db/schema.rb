85a4cffbe474d5c3e58247575249a56546bc1bfc
stephanesdr <stadlerstephane@gmail.com>
2019-04-18 20:01:23 +0200

    .

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 2019_04_18_140518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.boolean "discount", default: false
+    t.integer "percentage_discount", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,14 +36,6 @@ ActiveRecord::Schema.define(version: 2019_04_18_140518) do
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.float "price"
-    t.boolean "discount", default: false
-    t.integer "percentage_discount", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
+
 end
