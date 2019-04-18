f1bab11f4cdaa3d01863d69a9142856fbe8ce6c1
MargVander <margauxvanderbeken@gmail.com>
2019-04-18 19:21:19 +0200

    changed has many items for has many items through category items

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,6 +11,8 @@
 #
 
 class Category < ApplicationRecord
-  # has_many items, dependent: :destroy
+  has_many :category_items, dependent: :destroy
+  has_many :items, through: :category_items
+
   validates :name, presence: true
 end
