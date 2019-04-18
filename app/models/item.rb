86741fe8e93f2e1319b645be9cc611b458f5d84d
AlexDNZ <paleozene@gmail.com>
2019-04-18 16:38:48 +0200

    Item Model generated Set association in item.rb with Category model and CategoryItem model Set validations Set instanciations test for Items

---
+++ app/models/item.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id                  :bigint(8)        not null, primary key
+#  description         :text
+#  discount            :boolean          default(FALSE)
+#  name                :string
+#  percentage_discount :integer          default(0)
+#  price               :float
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
+class Item < ApplicationRecord
+  has_many :category_items#, dependent: :destroy
+  has_many :categories, through: :category_items
+  validates :price, numericality: { only_float: true, greater_than_or_equal_to: 0 }
+  validates :percentage_discount, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+end
---
+++ db/migrate/20190418140518_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :description
+      t.float :price
+      t.boolean :discount, :default => false
+      t.integer :percentage_discount, :default => 0
+
+      t.timestamps
+    end
+  end
+end
