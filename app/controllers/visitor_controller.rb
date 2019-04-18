d0942fb223b0a2ae71d856d54bf9b5d6467fc666
stephanesdr <stadlerstephane@gmail.com>
2019-04-18 19:11:20 +0200

    rename Visitor module into Home

--- app/controllers/visitor_controller.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class VisitorController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  root 'home/items#index'
+
+  namespace 'home' do
+    get '/', to: 'items#index'
+    resources :items
+  end
 end
