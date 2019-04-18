9ba3fe5bd2a938c36d6e834051a73f3fcef0c199
AlexDNZ <paleozene@gmail.com>
2019-04-18 16:42:04 +0200

    rubocop cleaning

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: items
@@ -13,7 +15,7 @@
 #
 
 class Item < ApplicationRecord
-  has_many :category_items#, dependent: :destroy
+  has_many :category_items, dependent: :destroy
   has_many :categories, through: :category_items
   validates :price, numericality: { only_float: true, greater_than_or_equal_to: 0 }
   validates :percentage_discount, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
--- db/migrate/20190418140518_create_items.rb
+++ db/migrate/20190418140518_create_items.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name
       t.text :description
       t.float :price
-      t.boolean :discount, :default => false
-      t.integer :percentage_discount, :default => 0
+      t.boolean :discount, default: false
+      t.integer :percentage_discount, default: 0
 
       t.timestamps
     end
