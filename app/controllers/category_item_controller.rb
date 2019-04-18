237d5dd0354a50d4225e22b077b90c3c7390d628
AlexDNZ <paleozene@gmail.com>
2019-04-18 20:16:12 +0200

    Creation of junction table CategoryItem Test (instanciation & association)for model Category Item

--- app/controllers/category_item_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class CategoryItemController < ApplicationController
-  belongs_to :item
-  belongs_to :category
-end
--- app/controllers/item_controller.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class ItemController < ApplicationController
-end
