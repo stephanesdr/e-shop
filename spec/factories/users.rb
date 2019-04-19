1d00a0825a6fc2b36b1873ac6eb32050149cbe52
stephanesdr <stadlerstephane@gmail.com>
2019-04-19 18:02:51 +0200

    add spec to user model devise

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -26,5 +26,9 @@
 
 FactoryBot.define do
   factory :user do
+    email { Faker::Internet.email }
+    password { "password" }
+    password_confirmation { "password" }
+    confirmed_at { Time.zone.today }
   end
 end
