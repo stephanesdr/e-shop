a915363445f775b4f86e442d3ccf260409a94324
MargVander <margauxvanderbeken@gmail.com>
2019-04-19 23:13:59 +0200

    added relation with profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one :cart, dependent: :destroy
+  has_one :profile, dependent: :destroy
   after_create :create_cart
 
   devise :database_authenticatable, :registerable,
