64189990db460d8536e884ec70b9fffe63301ff6
AlexDNZ <paleozene@gmail.com>
2019-04-19 15:33:16 +0200

    Creation of Cart model Creation of Carts controller Primal specs for Cart model Set association between user and cart in one to one relationship Create_cart method defined and set in user model

---
+++ spec/factories/carts.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :bigint(8)        not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint(8)
+#
+# Indexes
+#
+#  index_carts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+
+FactoryBot.define do
+  factory :cart do
+    user_id { 1 }
+  end
+end
