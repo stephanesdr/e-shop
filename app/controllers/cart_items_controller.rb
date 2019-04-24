b9b7242b42fe44795cf3d0f95d99c5f35733f1db
AlexDNZ <paleozene@gmail.com>
2019-04-24 17:07:27 +0200

    creation of a button add to cart OP

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class CartItemsController < ApplicationController
+  def update
+    @item = Item.find(params[:id])
+    CartItem.create(cart_id: current_user.id, item_id: @item.id)
+    flash[:notice] = "#{@item.name} a été ajouté au panier"
+  end
+
   def destroy
     @cart_item = CartItem.find(params[:id])
     CartItem.delete(@cart_item)
+    flash[:notice] = "#{@cart_item.item.name} a été retiré de votre panier"
     redirect_to request.referer
   end
 end
