05e8bbbc4ba864f1568dfaf0c200147d25454f6d
AlexDNZ <paleozene@gmail.com>
2019-04-24 14:42:05 +0200

    Delete items from stock Destroy method in items controller Delete button in items admin table

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class ItemsController < ApplicationController
+  # before_action :authenticate_admin! only: [:destroy]
+
   def index
     @items = Item.all
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    CartItem.where(item_id: @item.id).each { |cart_item| CartItem.delete(cart_item) }
+    CategoryItem.where(item_id: @item.id).each { |category_item| CategoryItem.delete(category_item) }
+    # OrderItem.where(item_id: @item.id).each {|order_item| OrderItem.delete(order_item) }
+    Item.delete(@item)
+    redirect_to request.referer
   end
 end
