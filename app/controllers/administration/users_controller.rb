3e565234fc6161654f5d82b6010315b693bb30ef
redcozmo <redcozmo@hotmail.com>
2019-04-24 20:21:27 +0200

    add update method and user edit button on dashboard

--- app/controllers/administration/users_controller.rb
+++ app/controllers/administration/users_controller.rb
@@ -2,8 +2,24 @@
 
 module Administration
   class UsersController < AdministrationController
+    before_action :set_user, only: [:update]
+
     def index
       @users = User.all
+    end
+
+    def update
+      @user = User.update(user_params)
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:id, :email, :admin)
     end
   end
 end
