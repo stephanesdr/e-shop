fb227c38334084badc035a79672833672050bd7a
MargVander <margauxvanderbeken@gmail.com>
2019-04-26 17:41:42 +0200

    added payment route

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     end
     resources :profiles
     resources :items, only: %i[index show]
+    resources :payments, only: %i[index]
   end
 
   namespace 'administration' do
