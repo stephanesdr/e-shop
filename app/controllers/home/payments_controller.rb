19aa1c21590e5312a9be82cccd493b260c425f15
AlexDNZ <paleozene@gmail.com>
2019-04-26 18:34:19 +0200

    set stripe inside payments view

--- app/controllers/home/payments_controller.rb
+++ app/controllers/home/payments_controller.rb
@@ -3,6 +3,9 @@
 module Home
   class PaymentsController < HomeController
     def index
+      @user = current_user
+      @cart = current_user.cart
+      @amount = @cart.sum_price * 100
       @cart = Cart.find(current_user.cart.id)
       @profile = Profile.find(current_user.profile.id)
     end
