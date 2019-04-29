9c8301f0c3ad720914b78b1cef4778c8c342742f
MargVander <margauxvanderbeken@gmail.com>
2019-04-29 18:56:24 +0200

    creation of method for confirmation order mail

--- app/models/order.rb
+++ app/models/order.rb
@@ -23,4 +23,9 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :item_orders, dependent: :destroy
   has_many :items, through: :item_orders
+  after_create :order_confirmation
+
+  def order_confirmation
+    UserMailer.order_email(self).deliver_now
+  end
 end
