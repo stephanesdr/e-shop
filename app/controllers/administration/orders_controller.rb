e500fe90a7a8574950e05df960a819db71f049b1
MargVander <margauxvanderbeken@gmail.com>
2019-04-30 16:11:27 +0200

    added mails method

--- app/controllers/administration/orders_controller.rb
+++ app/controllers/administration/orders_controller.rb
@@ -15,6 +15,12 @@ module Administration
       @order = Order.find(params[:id])
       @order.update(status: params[:order][:status].to_i)
       redirect_to administration_orders_path
+      puts @order.status
+      if @order.status == "shipped"
+        UserMailer.shipped_email(@order).deliver_now
+      elsif @order.status == "delivered"
+        UserMailer.delivered_email(@order).deliver_now
+      end
     end
   end
 end
