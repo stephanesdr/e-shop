b8701aa8b6a06eb66148983762fe0a8aa392e16d
redcozmo <redcozmo@hotmail.com>
2019-05-02 15:29:36 +0200

    add service for scrapp data and seed modifications

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,45 +10,54 @@
 
 require 'faker'
 
-# 1.upto(10) do |i|
-#   User.create!(
-#     email: Faker::Internet.email,
-#     password: Faker::Internet.password(8),
-#     confirmed_at: Time.zone.now
-#   )
-#   p "User #{i} : créé"
-# end
-#
-# 1.upto(10) do |i|
-#   Item.create!(
-#     price: Faker::Number.decimal(2),
-#     name: Faker::Commerce.unique.product_name,
-#     description: Faker::GreekPhilosophers.quote,
-#     discount: false,
-#     percentage_discount: 0
-#   )
-#   p "Item #{i} : créé"
-# end
-#
-# 1.upto(10) do |i|
-#   Category.create!(
-#     name: Faker::Commerce.department(1, true)
-#   )
-#   p "Category #{i} : créé"
-# end
-#
-# 1.upto(10) do |i|
-#   CategoryItem.create!(
-#     item_id: Item.all.sample.id,
-#     category_id: Category.all.sample.id
-#   )
-#   p "ItemCategory #{i} : créé"
-# end
-#
-# 1.upto(20) do |i|
-#   CartItem.create!(
-#     item_id: Item.all.sample.id,
-#     cart_id: Cart.all.sample.id
-#   )
-#   p "CartItem #{i} : créé"
-# end
+# == seed some users ==
+puts 'Seeding users...'
+1.upto(10) do |i|
+  email = Faker::Internet.email
+  next p "User already exist" if User.find_by(email: email)
+
+  User.create!(
+    email: email,
+    password: Faker::Internet.password(8),
+    confirmed_at: Time.zone.now
+  )
+  p "User #{i} : created"
+end
+
+# == seed an admin ==
+puts 'Seeding admin...'
+if !User.find_by(email: "admin@e-shop.com")
+  User.create!(
+    email: "admin@e-shop.com",
+    password: "superpassword",
+    confirmed_at: Time.zone.now
+  )
+  p "Admin created"
+else
+  p "Admin already exist"
+end
+
+# == scrapping phytodata ==
+# Use of service scrap_phytodb.rb in app/services/
+# Initialize scrapping : true = scrapping, false = initialize without scrapping
+# For only seed from csv file, just uncomment ScrapPhytodb.new and scrap.seed_from_csv
+
+# be carful which lign are comment or uncomment
+scrap = ScrapPhytodb.new(false)
+scrap.delete_items_categories
+# scrap.perform
+# scrap.seed_items_categories
+# scrap.seed_image_url
+# scrap.export_to_csv
+scrap.seed_from_csv("phyto_db_2019-05-01_23:54:22.csv")
+# == -------------------- ==
+
+# == seed some carts ==
+puts 'Seeding CartItems...'
+1.upto(20) do |i|
+  CartItem.create!(
+    item_id: Item.all.sample.id,
+    cart_id: Cart.all.sample.id
+  )
+  p "CartItem #{i} : created"
+end
