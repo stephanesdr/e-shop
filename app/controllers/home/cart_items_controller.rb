073af362aad762c230962196288f1b48ef195f54
MargVander <margauxvanderbeken@gmail.com>
2019-05-03 19:12:09 +0200

    updated create method and added edit method

--- app/controllers/home/cart_items_controller.rb
+++ app/controllers/home/cart_items_controller.rb
@@ -2,20 +2,28 @@
 
 module Home
   class CartItemsController < HomeController
-    def update
-      @item = Item.find(params[:id])
+    def create
+      @item = Item.find(params[:item_id])
       quantity = if params[:quantity].to_i.zero?
                    1
                  else
                    params[:quantity].to_i
                  end
       CartItem.create(cart_id: current_user.cart.id, item_id: @item.id, quantity: quantity)
-      # redirect_to request.referer
-      # flash[:notice] = "#{@item.name} a été ajouté au panier"
+
       respond_to do |format|
         format.js
         flash[:notice] = "#{@item.name} a été ajouté au panier"
-        format.html { redirect_to root_path }
+        format.html { redirect_to request.referer }
+      end
+    end
+
+    def update
+      @cart_item = CartItem.find(params[:id])
+      @cart_item.update(quantity: params[:cart_item][:quantity].to_i)
+      respond_to do |format|
+        format.js
+        format.html { redirect_to request.referer }
       end
     end
 
