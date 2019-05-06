4543f0320997bbe7be544f846a0bdabf65d7a531
stephanesdr <stadlerstephane@gmail.com>
2019-05-06 21:22:37 +0200

    add css to pagination and improve featured content

--- app/controllers/home/landing_controller.rb
+++ app/controllers/home/landing_controller.rb
@@ -4,7 +4,7 @@ module Home
   class LandingController < HomeController
     def index
       @categories = Category.all
-      @items = Item.all
+      @items = Item.page(params[:page])
       @most_popular_items = Item.most_popular(5)
       return unless current_user
 
