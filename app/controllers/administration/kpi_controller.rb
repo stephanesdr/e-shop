77160fd78be91e8ac9b8d0635ae0328b9400fcb9
AlexDNZ <paleozene@gmail.com>
2019-05-07 17:17:02 +0200

    update charts

--- app/controllers/administration/kpi_controller.rb
+++ app/controllers/administration/kpi_controller.rb
@@ -2,6 +2,19 @@
 
 module Administration
   class KpiController < AdministrationController
-    def index; end
+    def index
+      @data_product_hash = {}
+      Item.all.each do |item|
+        @data_product_hash[item.name] = (item.item_orders.count.to_f / ItemOrder.all.count.to_f) * 100 if item.item_orders.count != 0
+      end
+
+      @data_gain_hash = Order.group_by_day(:created_at).count
+      @data_gain_hash.each_key do |key|
+        Order.where(created_at: key.midnight..key.end_of_day).each do |order|
+          @data_gain_hash[key] += order.items.sum("price")
+        end
+      end
+      # @data_gain_hash.each_key { |key| key = key.strftime("%a %d %b") }
+    end
   end
 end
