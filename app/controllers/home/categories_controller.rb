dda4220fb32ef39a189703e219d650b3cff1a010
MargVander <margauxvanderbeken@gmail.com>
2019-05-09 19:18:27 +0200

    updated search method

--- app/controllers/home/categories_controller.rb
+++ app/controllers/home/categories_controller.rb
@@ -10,6 +10,8 @@ module Home
       @categories = Category.all
       @category = Category.find(params[:id])
       @items = @category.items.page(params[:page])
+      return unless current_user
+
       @products = []
       @cart = Cart.find(current_user.cart.id)
       CartItem.where(cart_id: @cart.id).each { |cart_item| @products << cart_item.item }
--- app/controllers/home/search_controller.rb
+++ app/controllers/home/search_controller.rb
@@ -2,14 +2,23 @@
 
 module Home
   class SearchController < HomeController
-    def create
+    def index
+      @categories = Category.all
       item_name = params[:search].capitalize
-      if Item.where(name: item_name).exists?
-        item = Item.find_by(name: item_name)
+      if Item.where("name like ?", "%#{item_name}%").exists? && Item.where("name like ?", "%#{item_name}%").count > 1
+        @items = Item.where("name like ?", "%#{item_name}%").page(params[:page])
+      elsif Item.where("name like ?", "%#{item_name}%").exists? && Item.where("name like ?", "%#{item_name}%").count == 1
+        item = Item.find_by("name like ?", "%#{item_name}%")
         redirect_to home_item_path(item.id)
       else
-        redirect_to home_search_index_path
+        flash[:alert] = "Nous n'avons pas pu acceder à votre requête."
+        redirect_to root_path
       end
+      return unless current_user
+
+      @products = []
+      @cart = Cart.find(current_user.cart.id)
+      CartItem.where(cart_id: @cart.id).each { |cart_item| @products << cart_item.item }
     end
   end
 end
