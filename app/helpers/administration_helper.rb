7f8ae6f944e4ed0843e31d5f058a9c325496912d
AlexDNZ <paleozene@gmail.com>
2019-05-10 18:06:34 +0200

    Admin authentification Set a check_i_admin method in administration helper Called in administration controllers Set a flash notice style for danger allert Adjust administration tests with login admin

--- app/helpers/administration_helper.rb
+++ app/helpers/administration_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module AdministrationHelper
+  def check_if_admin
+    return unless current_user.admin?
+
+    redirect_to root_path
+    flash[:alert] = "Seuls les admins ont accès à cette partie du site."
+  end
 end
