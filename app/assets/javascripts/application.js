6ab752a5314e86ab5bca809c60d89bbd9b1a831f
redcozmo <redcozmo@hotmail.com>
2019-05-10 20:59:20 +0200

    midify num column for searchBarOrders js function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -59,7 +59,7 @@
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[3];
+      td = tr[i].getElementsByTagName("td")[2];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
