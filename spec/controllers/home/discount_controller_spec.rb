4ac65221ac5633f83cffc449f9982fb80726cf54
MargVander <margauxvanderbeken@gmail.com>
2019-05-10 18:33:11 +0200

    creation of discount controller

---
+++ spec/controllers/home/discount_controller_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Home::DiscountController, type: :controller do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
